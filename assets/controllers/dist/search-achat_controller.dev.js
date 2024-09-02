0429342d906dad47a89ebcf9a3ae167685740471
JulienLeNoach <julien.lenoach77@gmail.com>
2024-09-02 16:36:44 +0200

    ajout msg alert pour annul_achat

--- assets/controllers/dist/search-achat_controller.dev.js
+++ assets/controllers/dist/search-achat_controller.dev.js
@@ -63,7 +63,7 @@ function (_Controller) {
     value: function attachEventListeners() {
       var table = document.querySelector('table');
       var footer = document.querySelector('footer');
-      var noResult = document.querySelector('#noResult'); // Vérifier si la table existe
+      var noResult = document.querySelector('#noResult'); // Garder la logique existante intacte
 
       table ? footer.scrollIntoView({
         behavior: 'smooth',
@@ -84,40 +84,53 @@ function (_Controller) {
           });
           var etatCell = row.cells[7];
           var etatAchatText = etatCell.textContent.replace(/\s+/g, '');
-          var selectedRow = document.querySelector('.selected');
 
-          if (etatAchatText == 'Validé') {
+          if (etatAchatText === 'Validé') {
             document.querySelectorAll('.valid, .reint').forEach(function (el) {
               return el.setAttribute('disabled', 'disabled');
             });
-          } else if (etatAchatText == 'Encours') {
+          } else if (etatAchatText === 'Encours') {
             document.querySelectorAll('.reint').forEach(function (el) {
               return el.setAttribute('disabled', 'disabled');
             });
-          } else if (etatAchatText = 'Annulé') {
+          } else if (etatAchatText === 'Annulé') {
             document.querySelectorAll('.annul, .valid, .edit').forEach(function (el) {
               return el.setAttribute('disabled', 'disabled');
             });
           }
 
           row.classList.add('selected');
-          btnElements.forEach(function (btn) {
-            btn.addEventListener('click', function () {
-              // Récupérer le lien et l'ID
-              var link = btn.getAttribute('data-link');
-              var action = btn.getAttribute('data-action');
-              var id = document.querySelector('.selected').getAttribute('data-id'); // Construire le message d'alerte
-              // let confirmationMessage = `Voulez-vous vraiment ${action} cet achat?`;
-              // Afficher l'alerte et rediriger si l'utilisateur confirme
-              // if (confirm(confirmationMessage)) {
-
-              console.log("confirm");
-              var detailLink = document.getElementById('detail');
-              detailLink.setAttribute('href', '/' + link + '/' + id);
-              window.location.href = detailLink.getAttribute('href'); // }
-            });
-          });
         });
+      }); // Ajouter l'écouteur d'événement pour les boutons, y compris l'alerte de confirmation
+
+      this.setupAlertForAnnulButton(btnElements);
+    }
+  }, {
+    key: "setupAlertForAnnulButton",
+    value: function setupAlertForAnnulButton(btnElements) {
+      btnElements.forEach(function (btn) {
+        // Vérifier si l'écouteur d'événement a déjà été ajouté
+        if (!btn.hasListener) {
+          btn.addEventListener('click', function (event) {
+            // Vérifier si le bouton cliqué est le bouton "Annuler un achat"
+            if (btn.getAttribute('data-action') === 'annuler') {
+              if (!confirm('Voulez-vous vraiment annuler cet achat?')) {
+                event.preventDefault(); // Annuler l'action si l'utilisateur ne confirme pas
+
+                return;
+              }
+            } // Exécuter l'action si l'utilisateur confirme
+
+
+            var link = btn.getAttribute('data-link');
+            var id = document.querySelector('.selected').getAttribute('data-id');
+            var detailLink = document.getElementById('detail');
+            detailLink.setAttribute('href', '/' + link + '/' + id);
+            window.location.href = detailLink.getAttribute('href');
+          }); // Marquer le bouton comme ayant déjà un écouteur d'événement
+
+          btn.hasListener = true;
+        }
       });
     }
   }]);
--- assets/controllers/search-achat_controller.js
+++ assets/controllers/search-achat_controller.js
@@ -1,44 +1,41 @@
+// search-achat_controller.js
 import { Controller } from '@hotwired/stimulus';
-
 
 export default class extends Controller {
     connect() {
-
         this.attachEventListeners();
-
     }
-    collapse() {
 
+    collapse() {
         let formContainer = document.querySelector('.form-container');
         let toggleButton = document.getElementById('toggleFormBtn');
         formContainer.classList.add('collapsed');
         toggleButton.innerHTML = '<span class="fr-icon-arrow-down-fill" aria-hidden="true"></span>';
 
-
-            toggleButton.addEventListener('click', function() {
-                if (formContainer.classList.contains('collapsed')) {
-                    formContainer.classList.remove('collapsed');
-                    toggleButton.innerHTML = '<span class="fr-icon-arrow-up-fill" aria-hidden="true"></span>';
-                } else {
-                    formContainer.classList.add('collapsed');
-                    toggleButton.innerHTML = '<span class="fr-icon-arrow-down-fill" aria-hidden="true"></span>';
-                }
-            });
-        
+        toggleButton.addEventListener('click', function() {
+            if (formContainer.classList.contains('collapsed')) {
+                formContainer.classList.remove('collapsed');
+                toggleButton.innerHTML = '<span class="fr-icon-arrow-up-fill" aria-hidden="true"></span>';
+            } else {
+                formContainer.classList.add('collapsed');
+                toggleButton.innerHTML = '<span class="fr-icon-arrow-down-fill" aria-hidden="true"></span>';
+            }
+        });
     }
     
     attachEventListeners() {
         let table = document.querySelector('table');
         let footer = document.querySelector('footer');
         let noResult = document.querySelector('#noResult');
-        // Vérifier si la table existe
+
+        // Garder la logique existante intacte
         table ? footer.scrollIntoView({ behavior: 'smooth', block: 'nearest' }) : 
         (noResult ? noResult.scrollIntoView({ behavior: 'smooth', block: 'nearest' }) : null);
 
         let rows = document.querySelectorAll('.clickable-row');
         let btnElements = document.querySelectorAll('#btn');
 
-        rows.forEach(function (row) {
+        rows.forEach((row) => {
             row.addEventListener('click', function () {
                 btnElements.forEach(function (btn) {
                     btn.removeAttribute('disabled');
@@ -46,44 +43,51 @@ export default class extends Controller {
                 rows.forEach(function (otherRow) {
                     otherRow.classList.remove('selected');
                 });
+
                 let etatCell = row.cells[7];
                 let etatAchatText = etatCell.textContent.replace(/\s+/g, '');
 
-                let selectedRow = document.querySelector('.selected');
-                        if (etatAchatText =='Validé') {
-
-                            document.querySelectorAll('.valid, .reint').forEach(el => el.setAttribute('disabled', 'disabled'));
-                        } else if (etatAchatText == 'Encours') {
-
-                            document.querySelectorAll('.reint').forEach(el => el.setAttribute('disabled', 'disabled'));
-                        } else if (etatAchatText = 'Annulé') {
-
-                            document.querySelectorAll('.annul, .valid, .edit').forEach(el => el.setAttribute('disabled', 'disabled'));
-                        }
+                if (etatAchatText === 'Validé') {
+                    document.querySelectorAll('.valid, .reint').forEach(el => el.setAttribute('disabled', 'disabled'));
+                } else if (etatAchatText === 'Encours') {
+                    document.querySelectorAll('.reint').forEach(el => el.setAttribute('disabled', 'disabled'));
+                } else if (etatAchatText === 'Annulé') {
+                    document.querySelectorAll('.annul, .valid, .edit').forEach(el => el.setAttribute('disabled', 'disabled'));
+                }
 
                 row.classList.add('selected');
-                btnElements.forEach((btn) => {
-                    btn.addEventListener('click', () => {
-                        // Récupérer le lien et l'ID
-                        let link = btn.getAttribute('data-link');
-                        let action = btn.getAttribute('data-action');
-                        let id = document.querySelector('.selected').getAttribute('data-id');
-                
-                        // Construire le message d'alerte
-                        // let confirmationMessage = `Voulez-vous vraiment ${action} cet achat?`;
-                
-                        // Afficher l'alerte et rediriger si l'utilisateur confirme
-                        // if (confirm(confirmationMessage)) {
-                            console.log("confirm");
-                            let detailLink = document.getElementById('detail');
-                            detailLink.setAttribute('href', '/' + link + '/' + id);
-                            window.location.href = detailLink.getAttribute('href');
-                        // }
-                    });
-                });
-
             });
         });
+
+        // Ajouter l'écouteur d'événement pour les boutons, y compris l'alerte de confirmation
+        this.setupAlertForAnnulButton(btnElements);
     }
 
+    setupAlertForAnnulButton(btnElements) {
+        btnElements.forEach((btn) => {
+            // Vérifier si l'écouteur d'événement a déjà été ajouté
+            if (!btn.hasListener) {
+                btn.addEventListener('click', (event) => {
+                    // Vérifier si le bouton cliqué est le bouton "Annuler un achat"
+                    if (btn.getAttribute('data-action') === 'annuler') {
+                        if (!confirm('Voulez-vous vraiment annuler cet achat?')) {
+                            event.preventDefault(); // Annuler l'action si l'utilisateur ne confirme pas
+                            return;
+                        }
+                    }
+
+                    // Exécuter l'action si l'utilisateur confirme
+                    let link = btn.getAttribute('data-link');
+                    let id = document.querySelector('.selected').getAttribute('data-id');
+
+                    let detailLink = document.getElementById('detail');
+                    detailLink.setAttribute('href', '/' + link + '/' + id);
+                    window.location.href = detailLink.getAttribute('href');
+                });
+
+                // Marquer le bouton comme ayant déjà un écouteur d'événement
+                btn.hasListener = true;
+            }
+        });
+    }
 }
